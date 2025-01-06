08b9f04e52c09b32b735ab5f880d14ef009c4487
Giorgos Stamelakis <georgios.stamelakis@atos.net>
2025-01-06 18:05:07 +0200

    Properly dismiss error view when entering background

--- Bookworm/Views/ErrorView.swift
+++ Bookworm/Views/ErrorView.swift
@@ -12,36 +12,44 @@ struct ErrorView: View {
     let message: String
     let duration: TimeInterval = 3
 
-    var body: some View {
-           if isVisible {
-               HStack(spacing: 10) {
-                   ZStack {
-                       Image(systemName: "exclamationmark.circle.fill")
-                           .font(.title2)
-                           .foregroundColor(.white)
-                   }
+    @Environment(\.scenePhase) private var scenePhase
 
-                   Text(message)
-                       .foregroundColor(.white)
-                       .multilineTextAlignment(.leading)
-                       .shadow(color: .black.opacity(0.6), radius: 2, x: 1, y: 1)
-               }
-               .padding()
-               .background(
-                   RoundedRectangle(cornerRadius: 12)
-                       .fill(Color.red)
-                       .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 2)
-               )
-               .padding(.horizontal, 16)
-               .transition(.opacity)
-               .onAppear {
-                   DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                       isVisible = false
-                   }
-               }
+    private func startDismissTimer() {
+           DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+               isVisible = false
            }
        }
 
+    var body: some View {
+        if isVisible {
+            HStack(spacing: 10) {
+                ZStack {
+                    Image(systemName: "exclamationmark.circle.fill")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                }
 
+                Text(message)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.leading)
+                    .shadow(color: .black.opacity(0.6), radius: 2, x: 1, y: 1)
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.red)
+                    .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 2)
+            )
+            .transition(.opacity)
+            .onAppear {
+                startDismissTimer()
+            }
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .background {
+                    isVisible = false
+                }
+            }
+        }
+    }
 
 }
